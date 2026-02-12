abfccd2b03ee093b0fafa9e79a987c9c24d79101
ayaminitrv25 <ayamini.trv25ec024@gecbh.ac.in>
2026-02-12 19:40:24 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("✅ Thank you for registering! We’ll contact you soon.");
+    form.reset();
+  });
+});
